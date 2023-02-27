d450dc16fcb9e077da72296af1205c1556cdf896
Vovan4ik <pologonik555@gmail.com>
2023-02-27 19:19:30 +0300

    Добавьте файлы проекта.

---
+++ aaauuu/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+using System.Timers;
+
+Console.Write("Time:");
+void time(int hour, int minute,int second)
+{
+    int timeCountDown = hour * 3600 + minute * 60 + second;
+    var timer = new System.Timers.Timer(1000);
+    timer.Elapsed += (sender, eventArgs) =>
+    {
+        if (timeCountDown == 1)
+        {
+            timer.Dispose();
+            Console.Clear();
+            Console.WriteLine("Ну кончился и что?");
+            return;
+        }
+        timeCountDown--;
+        Console.Clear();
+        Console.WriteLine($"Осталось {timeCountDown} секунды");
+    };
+    timer.Start();
+    Console.ReadLine();
+    Console.WriteLine("Повторить или выйти?");
+    var choose = Console.ReadLine();
+    if (choose.Contains("Повт"))
+    {
+        Console.WriteLine("Часы,минуты,Секунды:");
+        hour = int.Parse(Console.ReadLine());
+        minute = int.Parse(Console.ReadLine());
+        second = int.Parse(Console.ReadLine());
+        time(hour, minute, second);
+        Console.ReadKey();
+    }
+    else
+    {
+        Console.WriteLine("Сам закрой");  
+    }
+    Console.ReadKey();
+}
+time(0,0,4);
